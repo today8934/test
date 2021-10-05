e6a0613664ac51ebf6c6594428297945956f6c6d
today8934 <today8934@gmail.com>
2021-10-05 16:21:06 +0900

    Test project

---
+++ src/main/java/study/test/Study.java
@@ -0,0 +1,4 @@
+package study.test;
+
+public class Study {
+}
---
+++ src/test/java/study/test/StudyTest.java
@@ -0,0 +1,36 @@
+package study.test;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StudyTest {
+
+    @Test
+    @DisplayName("스터디만들기")
+    void create_new_study() {
+        Study study = new Study();
+        assertNotNull(study);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+}
